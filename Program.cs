3449e186ce7dc11164971f1feb3b086ad392bc54
Wesley Josimar de Lima <297780@unoesc.lan>
2024-08-14 22:07:12 -0300

    Primeniro Projeto .net

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//comentario de uma linha
+/*
+
+comentario de
+multiplas linhas
+
+*/
+// declarando variavel
+using System.Diagnostics.CodeAnalysis;
+
+int number;
+// atribuindo valor
+11 number = 10;
+
+//declarando constante
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo string concatenada
+Console.WindowHeight( $"A semana tem (DAY_IN_WEEK) dias");
