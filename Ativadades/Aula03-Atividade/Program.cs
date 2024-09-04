05373effa79ba509931f900ea33d32bb8023df95
wesleyjosimar <wesleyjosimar3@gmail.com>
2024-09-04 19:31:42 -0300

    Aula03, atividade Array

---
+++ Ativadades/Aula03-Atividade/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] pares = new int[50];
+int indicePar = 0;
+
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        pares[indicePar] = i;
+        indicePar++;
+    }
+}
+
+Console.WriteLine("Números pares:");
+for (int i = 0; i < pares.Length; i++)
+{
+    Console.Write(pares[i] + " ");
+}
