6d78bc1931e8b00a627b66161959a59dca8be1db
wesleyjosimar <wesleyjosimar3@gmail.com>
2024-09-10 20:00:45 -0300

    Gamemap pagina 40 e 42

---
+++ Ativadades/GameMap/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using GameMap;
+
+Terrain.TerrainEnum[,] map =
+{
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS },
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS },
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS },
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS, Terrain.TerrainEnum.GRASS,
+    Terrain.TerrainEnum.GRASS },
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.WALL,
+    Terrain.TerrainEnum.WALL },
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND },
+    { Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND },
+    { Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND },
+    { Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.SAND,
+    Terrain.TerrainEnum.SAND },
+    { Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER },
+    { Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER, Terrain.TerrainEnum.WALL, Terrain.TerrainEnum.WATER,
+    Terrain.TerrainEnum.WATER }
+};
+Console.OutputEncoding = UTF8Encoding.UTF8;
+for (int row = 0; row < map.GetLength(0); row++)
+{
+    for (int column = 0; column < map.GetLength(1); column++)
+    {
+        Console.ForegroundColor = map[row, column].GetColor();
+        Console.Write(map[row, column].GetChar() + " ");
+    }
+    Console.WriteLine();
+}
+Console.ForegroundColor = ConsoleColor.Gray;
---
+++ Ativadades/GameMap/Terrain.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameMap
+{
+    public static class Terrain
+    {
+        public enum TerrainEnum
+        {
+            GRASS,
+            SAND,
+            WATER,
+            WALL
+        }
+
+        public static ConsoleColor GetColor(this TerrainEnum terrain)
+        {
+            switch (terrain)
+            {
+                case TerrainEnum.GRASS: return ConsoleColor.Green;
+                case TerrainEnum.SAND: return ConsoleColor.Yellow;
+                case TerrainEnum.WATER: return ConsoleColor.Blue;
+                default: return ConsoleColor.DarkGray;
+            }
+        }
+        public static char GetChar(this TerrainEnum terrain)
+        {
+            switch (terrain)
+            {
+                case TerrainEnum.GRASS: return '\u201c';
+                case TerrainEnum.SAND: return '\u25cb';
+                case TerrainEnum.WATER: return '\u2248';
+                default: return '\u25cf';
+            }
+        }
+    }
+}
